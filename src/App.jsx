58ca5c1f3d46c238cdd3613dd0abd296929a3eaf
Joaquin Armand <djoaquinaa@gmail.com>
2024-09-18 21:31:17 -0300

    Eliminar import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import NavBar from './components/navBar/NavBar';
 import Carrusel from './components/carrusel/Carrusel';
 import  Cards from './components/cards/cards';
 import Footer from './components/footer/footer';
-import  CardPromotion  from './components/CardPromotion/cardPromotion';
 
 
 function App() {
