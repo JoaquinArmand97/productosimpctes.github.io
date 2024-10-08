c3236b1bb7a5c23e9e7e20c10c281fd4e0f4f566
Joaquin Armand <djoaquinaa@gmail.com>
2024-10-08 16:57:13 -0300

    ruta home reparada

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Productos } from '../pages/Productos'; // Verifica si es mayúscula o minúscula
 import { Contacto } from '../pages/contacto'; // Asegúrate de que este nombre coincide
-import Home from '../pages/Home'; // Asegúrate de que la 'H' esté bien escrita
+import Home from '../pages/home'; // Asegúrate de que la 'H' esté bien escrita
 import NavBar from '../components/navBar/NavBar';
 import CardItem from '../components/cardItem/CardItem'; // Verifica el nombre del componente
 import Category from '../pages/Category';
